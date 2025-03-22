aa9dbee977589aaf35eec0cc8a36227f21cee2ac
DaniVasques <roballo.daniela13@gmail.com>
2025-03-22 18:13:51 -0300

    Instalação do bootstrap, criação de componente e navegação de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { CadastroProdutosComponent } from './components/pages/cadastro-produtos/cadastro-produtos.component';
+import { ConsultaProdutosComponent } from './components/pages/consulta-produtos/consulta-produtos.component';
+import { EdicaoProdutosComponent } from './components/pages/edicao-produtos/edicao-produtos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'pages/cadastro-produtos',//caminho da rota
+        component: CadastroProdutosComponent//componente que será exibido
+    },
+    {
+        path: 'pages/consulta-produtos',
+        component: ConsultaProdutosComponent
+    },
+    {
+        path: 'pages/edicao-produtos',
+        component: EdicaoProdutosComponent
+    },
+    {
+        path: '', pathMatch: 'full',
+        redirectTo: 'pages/consulta-produtos'
+    }
+];
